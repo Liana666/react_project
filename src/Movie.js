06febe88a6387881aecfdec4163052d042177a3c
Liana666 <78962820+Liana666@users.noreply.github.com>
2021-09-16 22:18:40 +0300

    add pagination

--- src/Movie.js
+++ src/Movie.js
@@ -1,40 +1,92 @@
 import React, { createElement, createRef, useEffect, useState } from "react";
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
+// import ReactPaginate from 'react-paginate';
+// import { width } from "dom-helpers";
+
 
 export default function Movie() {
-
-    const [hasError, setErrors] = useState(false);
+    const [isLoaded, setisLoaded] = useState(false);
     const [movie, addMovie] = useState([]);
-
     let inputValue = React.createRef();
-
     const imgSrc = 'https://image.tmdb.org/t/p/original/';
 
+
+
     function getMovie() {
-        fetch(`https://api.themoviedb.org/3/search/movie?query=${inputValue.current.value}&api_key=d495d35d47c329b48d81d83ed0f10265`)
+        fetch(`https://api.themoviedb.org/3/search/movie?page=1&query=${inputValue.current.value}&api_key=d495d35d47c329b48d81d83ed0f10265`)
             .then((response) => {
                 return response.json();
 
             })
             .then((data) => {
                 addMovie(data.results);
-                console.log(data.results);
+                console.log(data)
+                setisLoaded(true);
 
             })
-            .catch((error) => setErrors(error));
+            .catch(error => console.error('Error', error));
     }
+
+    function getPage(e) {
+        let page = e.target.innerHTML;
+
+        fetch(`https://api.themoviedb.org/3/search/movie?page=${page}&query=${inputValue.current.value}&api_key=d495d35d47c329b48d81d83ed0f10265`)
+            .then((response) => {
+                return response.json();
+
+            })
+            .then((data) => {
+                addMovie(data.results);
+                console.log(data)
+                setisLoaded(true);
+
+            })
+            .catch(error => console.error('Error', error));
+
+        window.scrollTo(0, 0);
+    }
+
+
 
 
     return (
         <div>
+
             <input type="text" ref={inputValue} />
             <button onClick={getMovie}>Search</button>
             <div className="out">
-                {movie.length !== 0 ? movie.map((item) => <li><img style={{ width: 150 }} src={`${imgSrc}${item.poster_path}`} /> {item.original_title}</li>) : 'No movies'}
-            </div>
-        </div>
+                <Row>
+                    {isLoaded ? (
+                        movie.map((item) => {
+                            return (
+                                <>
+                                    <Col className="cart" xl={8} md={12} xs={24}>
+                                        <img style={{ width: '100%' }} src={`${imgSrc}${item.poster_path}`} alt="" />
+                                        {item.original_title}
+                                    </Col>
+                                </>
+                            )
+
+
+                        })
+                    ) : (
+                        <div></div>
+                    )}
+
+                </Row>
+            </div >
+
+            {isLoaded ? (
+                <div style={{ display: "flex", marginTop: "15px" }}>
+                    <div className='page' style={{ width: '30px', height: '30px' }} onClick={getPage}>1</div>
+                    <div className='page' style={{ width: '30px', height: '30px' }} onClick={getPage}>2</div>
+                    <div className='page' style={{ width: '30px', height: '30px' }} onClick={getPage}>3</div>
+                </div>
+            ) : (<div></div>)
+            }
+        </div >
     )
-
-
 
 
 }
